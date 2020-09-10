5b3cf21822a8c5154f6c9756df627e3b063f709a
quaiyyumfaisal <inanfaisal13@gmail.com>
2020-09-11 03:31:32 +0600

    ProgrammingAssignment2.R

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) inv <<- solve
+    getsolve <- function() inv
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+} 
+
+cacheSolve <- function(x, ...) {
+    inv <- x$getsolve()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setsolve(inv)
+    inv
+}
